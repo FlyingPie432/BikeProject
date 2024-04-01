d92baaf96e3c3ee3a6dfeef759b5ca98b39ea1e3
jamal <polchayevc@gmail.com>
2024-04-01 23:23:17 +0400

    configured send_email

--- app/views/index.py
+++ app/views/index.py
@@ -1,6 +1,7 @@
 import http.client
 
 from django.contrib import messages
+from django.core.exceptions import ValidationError
 from django.core.mail import send_mail
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
@@ -71,15 +72,20 @@ def send_email(request):
         name = request.POST.get('name')
         email = request.POST.get('email')
         message = request.POST.get('message')
+        try:
+            send_mail(
+                subject='Message from Volti Website',
+                message=f'Name: {name}\nEmail: {email}\nMessage: {message}',
+                from_email=email,
+                recipient_list=['your_email@example.com'],
+                fail_silently=False,
+            )
+            messages.success(request, 'Email successfully sent!')
+        except ValidationError as e:
+            messages.error(request, 'Email validation error occurred. Please try again.')
+        except Exception as e:
+            messages.error(request, 'An error occurred while sending the email. Please try again later.')
+        return HttpResponseRedirect(reverse('contactus'))
 
-        send_mail(
-            subject='Message from Volti Website',
-            message=f'Name: {name}\nEmail: {email}\nMessage: {message}',
-            from_email=email,
-            recipient_list=['your_email@example.com'],
-            fail_silently=False,
-        )
-        messages.success(request, 'Email successfully sent!')
-        return HttpResponseRedirect(reverse('index'))
+    return HttpResponseRedirect(reverse('contactus'))
 
-    return render(request, 'contacts.html')
